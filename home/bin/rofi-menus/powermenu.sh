32cf2c28282b94b7a33d63caa29cef17a11f86ff
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2024-08-25 19:49:58 +0300

    Fixed the path to the screen lock script

--- home/bin/rofi-menus/powermenu.sh
+++ home/bin/rofi-menus/powermenu.sh
@@ -3,9 +3,9 @@
 choice=$(printf "  Lock\n󰍃  Logout\n󰒲  Suspend\n  Reboot\n  Shutdown" | rofi -dmenu)
 
 case "$choice" in
-  "  Lock") sh $HOME/bin/screen-lock ;;
+  "  Lock") sh $HOME/bin/screen-lock.sh ;;
   "󰍃  Logout") pkill -KILL -u "$USER" ;;
-  "󰒲  Suspend") systemctl suspend && sh $HOME/bin/screen-lock ;;
+  "󰒲  Suspend") systemctl suspend && sh $HOME/bin/screen-lock.sh ;;
   "  Reboot") systemctl reboot ;;
   "  Shutdown") systemctl poweroff ;;
 esac
