89e0110721acfdc13a149c19181f005595224456
DIMFLIX-OFFICIAL <dimflix.official@gmail.com>
2025-03-23 13:50:35 +0300

    Optimized package list to speed up installation
    Removed redundant and optional packages:
    - Packages already included in base-devel: automake, fakeroot, gcc, gzip, make
    - Moved to manual selection: timeshift, evince
    - Dependencies not needed (if user needs them, he will install them himself): pacman-contrib, downgrade, parallel, dpkg, gnu-netcat, mat2, reflector, clang, cmake, uthash, shellcheck, usbutils, netctl, gparted, libreoffice, archivers (zip/unzip/p7zip/unrar), cowsay, pyenv, lxappearance.
    
    Moved wmname to the bspwm section (not required for Hyprland)

--- Builder/packages.py
+++ Builder/packages.py
@@ -2,34 +2,80 @@ from utils.schemes import DistributionPackages, PackageInfo, Packages
 
 BASE = Packages(
 	pacman=DistributionPackages(
-		common=[
-			"pacman-contrib", "downgrade", "libnotify", "ffmpeg","ffmpegthumbnailer", "jq", "parallel", "kitty", "fastfetch", "lsd", "bat", "brightnessctl", 
-			"automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "firefox", "fish", "fisher", "dpkg", "gcc", "git", "gnu-netcat", "btop", 
-			"micro", "mat2", "nemo", "papirus-icon-theme", "pavucontrol", "pamixer", "pipewire", "pipewire-pulse", "pipewire-audio",
-			"pipewire-jack", "pipewire-alsa", "wireplumber", "python-pyalsa", "ranger", "redshift", "reflector", "sudo", "tree", "unrar",
-			"zip", "unzip", "uthash", "ark", "cmake", "clang", "gzip", "imagemagick",
-			"make", "openssh", "shellcheck", "vlc", "loupe", "usbutils", "openvpn", "networkmanager-openvpn", "p7zip", "gparted",
-			"sshfs", "wget", "netctl", "libreoffice", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
-        	"ttf-iosevka-nerd", "playerctl", "starship", "upower", "udiskie", "zenity", "gvfs", "qt5ct", "qt6ct",
-        	"timeshift", "sddm", "qt5-graphicaleffects", "qt5-svg",  "qt5-quickcontrols2", "clipnotify",
-			"xdg-desktop-portal-gtk", "gnome-disk-utility", "evince", "neovim", "tmux", "cowsay", "polkit-gnome",
-			"rofimoji", "wmname", "pyenv", "xdg-desktop-portal", "ttf-hack-nerd", "networkmanager", "noto-fonts", 
-            "noto-fonts-cjk", "noto-fonts-emoji", "noto-fonts-extra"
+		common=[  
+			##==> Базовые инструменты и демоны
+			###########################################
+			"base-devel", "git", "networkmanager", "libnotify", "bluez", 
+            "bluez-utils", "playerctl", "upower", "brightnessctl", 
+			"udiskie", "xdg-desktop-portal-gtk", "xdg-desktop-portal",
+
+			##==> Звук
+            ###########################################
+            "pipewire-jack", "pipewire-alsa", "wireplumber", 
+			"python-pyalsa", "pavucontrol", "pamixer", "pipewire", 
+			"pipewire-pulse", "pipewire-audio",
+            
+			##==> CLI-Tools
+			###########################################
+            "jq", "fastfetch", "lsd", "bat", "micro", "sudo",
+            "fish", "fisher", "btop", "ranger", "starship",
+            "openssh", "sshfs", "wget", "neovim", "tmux",
+			"ffmpeg", "clipnotify", "udiskie", "tree",
+
+			##==> GUI 
+			###########################################
+			"sddm", "firefox", "kitty", "blueman", "ark", "nemo", "gvfs", 
+			"ffmpegthumbnailer", "imagemagick", "vlc", "loupe", "dunst", 
+			"qt5ct", "qt6ct", "qt5-graphicaleffects", "qt5-svg", 
+			"qt5-quickcontrols2", "redshift", "zenity", "polkit-gnome",
+			"gnome-disk-utility","rofimoji", 
+            
+			##==> Шрифты
+            ###########################################
+            "ttf-hack-nerd", "noto-fonts", "noto-fonts-cjk", 
+			"noto-fonts-emoji", "noto-fonts-extra", "ttf-iosevka-nerd", 
+			"ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", 
+			"ttf-fira-code",
+	
+			##==> Сеть/VPN
+            ###########################################
+			"openvpn", "networkmanager-openvpn", 
 		],
-		bspwm_packages=["xorg", "bspwm", "sxhkd", "xorg-xinit", "xclip", "feh", "lxappearance", "polybar", "xorg-xrandr", "xsettingsd"],
+		bspwm_packages=[
+			"xorg", "bspwm", "sxhkd", "xorg-xinit", "xclip", "feh", 
+			"wmname", "polybar", "xorg-xrandr", "xsettingsd"
+		],
 		hyprland_packages=[
 			"hyprland", "waybar", "swww", "cliphist", "wl-clipboard", "xdg-desktop-portal-hyprland", "qt5-wayland", "qt6-wayland",
 			"xdg-desktop-portal-wlr", "hypridle"]
 	),
 	aur=DistributionPackages(
 		common=[
-			"gnome-calculator-gtk3", "flameshot-git", "rofi-lbonn-wayland-git", "bibata-cursor-theme-bin", "tela-circle-icon-theme-dracula",
-			"themix-theme-oomox-git", "themix-plugin-base16-git", "themix-gui-git", "themix-export-spotify-git",
-			"themix-theme-materia-git", "oomox-qt5-styleplugin-git", "oomox-qt6-styleplugin-git", "cava", "pokemon-colorscripts",
-			"youtube-dl", "update-grub", "ttf-meslo-nerd-font-powerlevel10k", "visual-studio-code-bin"
+			##==> GUI
+            ###########################################
+            "gnome-calculator-gtk3", "flameshot-git", "themix-gui-git", 
+            "visual-studio-code-bin", "rofi-lbonn-wayland-git",
+            
+            ##==> Кастомизация: Темы, иконки и курсор
+            ###########################################
+            "bibata-cursor-theme-bin", "tela-circle-icon-theme-dracula",
+            "themix-theme-oomox-git", "themix-plugin-base16-git", 
+            "themix-export-spotify-git", "themix-theme-materia-git",
+            "oomox-qt5-styleplugin-git", "oomox-qt6-styleplugin-git",
+            
+            ##==> CLI-Tools
+            ###########################################
+            "update-grub", "cava", "youtube-dl", "pokemon-colorscripts",
+            
+            ##==> Шрифты
+            ###########################################
+            "ttf-meslo-nerd-font-powerlevel10k",
 		],
 		bspwm_packages=["i3lock-color", "picom-ftlabs-git"],
-		hyprland_packages=["hyprpicker", "swaylock-effects-git", "wlr-randr-git", "hyprprop", "grimblast-git"]
+		hyprland_packages=[
+			"hyprpicker", "swaylock-effects-git", "wlr-randr-git", 
+			"hyprprop", "grimblast-git"
+		]
 	)
 )
 
@@ -38,8 +84,9 @@ DRIVERS = {
 		pacman=DistributionPackages(
 			common=[
 				"lib32-mesa", "vulkan-intel", "lib32-vulkan-intel", 
-				"vulkan-icd-loader", "lib32-vulkan-icd-loader", "intel-media-driver",
-				"libva-intel-driver", "xf86-video-intel"
+				"vulkan-icd-loader", "lib32-vulkan-icd-loader", 
+				"intel-media-driver", "libva-intel-driver", 
+				"xf86-video-intel"
 			]
 		)
 	),
@@ -55,14 +102,18 @@ DRIVERS = {
 		pacman=DistributionPackages(
 			common=[
 				"nvidia-dkms", "nvidia-utils", "lib32-nvidia-utils",
-				"nvidia-settings", "vulkan-icd-loader", "lib32-vulkan-icd-loader",
-				"lib32-opencl-nvidia", "opencl-nvidia", "libxnvctrl"
+				"nvidia-settings", "vulkan-icd-loader", "opencl-nvidia",
+				"lib32-vulkan-icd-loader", "lib32-opencl-nvidia",
+				"libxnvctrl"
 			]
 		)
 	)
 }
 
 CUSTOM = {
+    "useful": {
+        "timeshift": PackageInfo("A system restore utility for Linux", recommended=True)
+	},
     "development": {
         "obsidian": PackageInfo("A powerful knowledge base that works on top of a local folder of plain text Markdown files", recommended=True),
         "postgresql": PackageInfo("Sophisticated object-relational DBMS", recommended=True),
@@ -86,6 +137,7 @@ CUSTOM = {
 	},
     "office": {
         "libreoffice-fresh": PackageInfo("Comprehensive office suite for word processing, spreadsheets, and presentations"),
-        "onlyoffice-bin": PackageInfo("Office suite that allows collaborative editing of documents", aur=True, recommended=True)
+        "onlyoffice-bin": PackageInfo("Office suite that allows collaborative editing of documents", aur=True, recommended=True),
+		"evince": PackageInfo("Document viewer", selected=True, recommended=True)
     }
 }
