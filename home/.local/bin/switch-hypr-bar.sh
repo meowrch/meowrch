eb6a99ff8d447ef9cbb85cfa63e5aefa47abd3fa
DIMFLIX <dimflix.official@gmail.com>
2025-05-27 21:56:43 +0300

    Made it normal to enable/disable/change status bars.

--- home/.local/bin/switch-hypr-bar.sh
+++ home/.local/bin/switch-hypr-bar.sh
@@ -9,16 +9,23 @@
 # The program was created by DIMFLIX
 # Github: https://github.com/DIMFLIX
 
-CONFIG_FILE="$HOME/.config/hypr/hyprland.conf"
+FLAG_FILE="$HOME/.config/hypr/current_bar"
 
-current_bar=$(grep -E '^exec-once = (waybar|mewline)\b' "$CONFIG_FILE" | grep -oE 'waybar|mewline')
+# Initialize the flag file if necessary
+[ ! -f "$FLAG_FILE" ] && echo "mewline" > "$FLAG_FILE"
+
+# Define current and new status bars
+current_bar=$(cat "$FLAG_FILE")
 new_bar=$([ "$current_bar" == "waybar" ] && echo "mewline" || echo "waybar")
 
-sed -i "s/^exec-once = $current_bar/exec-once = $new_bar/" "$CONFIG_FILE"
+# Update the flag file
+echo "$new_bar" > "$FLAG_FILE"
 
-
+# Stop the current bar
 pkill -x "$current_bar"
-nohup "$new_bar" >/dev/null 2>&1 &
+
+# Run the new bar through a single script
+nohup "${XDG_BIN_HOME:-$HOME/.local/bin}/toggle-bar.sh --start" >/dev/null 2>&1 &
 disown
 
 exit 0
