9de65e11e664842ddcc2983e2c20f49b14adb8e8
DIMFLIX <dimflix.official@gmail.com>
2025-05-28 15:27:16 +0300

    Fixed a bug with moving the window to and from silent workspace

---
+++ home/.local/bin/toggle-hypr-sworkspace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ┏━━━┳━━┳━┓┏━┳━━━┳┓╋╋┏━━┳━┓┏━┓
+# ┗┓┏┓┣┫┣┫┃┗┛┃┃┏━━┫┃╋╋┗┫┣┻┓┗┛┏┛
+# ╋┃┃┃┃┃┃┃┏┓┏┓┃┗━━┫┃╋╋╋┃┃╋┗┓┏┛
+# ╋┃┃┃┃┃┃┃┃┃┃┃┃┏━━┫┃╋┏┓┃┃╋┏┛┗┓
+# ┏┛┗┛┣┫┣┫┃┃┃┃┃┃╋╋┃┗━┛┣┫┣┳┛┏┓┗┓
+# ┗━━━┻━━┻┛┗┛┗┻┛╋╋┗━━━┻━━┻━┛┗━┛
+# The program was created by DIMFLIX
+# Github: https://github.com/DIMFLIX
+
+# Check if the active window is on a special worxpace
+if hyprctl activewindow | grep -q "special:special"; then
+    hyprctl dispatch movetoworkspace $(hyprctl activeworkspace | awk '{print $3}')
+else
+    hyprctl dispatch movetoworkspacesilent special
+fi
+
