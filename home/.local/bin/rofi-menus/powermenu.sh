b17320333e0141c81b320fbde978035564ca9ef0
DIMFLIX <dimflix.official@gmail.com>
2025-12-09 19:12:17 +0300

    Added correct exit from bspwm to powermenu

--- home/.local/bin/rofi-menus/powermenu.sh
+++ home/.local/bin/rofi-menus/powermenu.sh
@@ -4,7 +4,13 @@ choice=$(printf "  Lock\n󰍃  Logout\n󰒲  Suspend\n  Reboot\n  Shutd
 
 case "$choice" in
   "  Lock") sh ${XDG_BIN_HOME:-$HOME/bin}/screen-lock.sh ;;
-  "󰍃  Logout") pkill -KILL -u "$USER" ;;
+  "󰍃  Logout") 
+    if [[ "$XDG_CURRENT_DESKTOP" == "bspwm" ]]; then
+      bspc quit
+    else
+      pkill -KILL -u "$USER"
+    fi
+    ;;
   "󰒲  Suspend") sh ${XDG_BIN_HOME:-$HOME/bin}/screen-lock.sh --suspend ;;
   "  Reboot") systemctl reboot ;;
   "  Shutdown") systemctl poweroff ;;
