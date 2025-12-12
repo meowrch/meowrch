d7e8dd302615de014053e310a1b60d603d533119
DIMFLIX <dimflix.official@gmail.com>
2025-12-12 16:16:39 +0300

    bspwm calculated the window size as a percentage of the screen size

--- home/.config/bspwm/default/windowrules.sh
+++ home/.config/bspwm/default/windowrules.sh
@@ -1,19 +1,35 @@
 #!/bin/sh
 
+# Obtaining screen dimensions
+SW=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f1)
+SH=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f2)
+
+# Function that calculates window dimensions based on percentages
+rect() {
+    w_pct=$1
+    h_pct=$2
+    
+    w=$((SW * w_pct / 100))
+    h=$((SH * h_pct / 100))
+    x=$(( (SW - w) / 2 ))
+    y=$(( (SH - h) / 2 ))
+    
+    echo "${w}x${h}+${x}+${y}"
+}
+
 bspc rule -a feh state=floating
 bspc rule -a '*:sun-awt-X11-XWindowPeer' manage=off
 bspc rule -a 'vlc' state=floating center=true
 bspc rule -a 'Blueman-manager' state=floating center=true
-bspc rule -a 'qt5ct' state=floating rectangle=900x600+0+0 center=true
-bspc rule -a 'qt6ct' state=floating rectangle=900x600+0+0 center=true
-bspc rule -a 'ark' state=floating rectangle=1200x800+0+0 center=true
-bspc rule -a 'Xarchiver' state=floating rectangle=1200x800+0+0 center=true
-bspc rule -a 'pavucontrol' state=floating center=true
+bspc rule -a 'qt5ct' state=floating center=true
+bspc rule -a 'qt6ct' state=floating center=true
+bspc rule -a 'ark' state=floating center=true
+bspc rule -a 'Xarchiver' state=floating center=true
 bspc rule -a 'Yad' state=floating center=true
 bspc rule -a 'Blueman-manager' state=floating center=true
-bspc rule -a 'org.gnome.FileRoller' state=floating rectangle=1200x800+0+0 center=true
-bspc rule -a 'Gnome-calculator' state=floating rectangle=360x500+0+0 center=true
-bspc rule -a 'loupe' state=floating rectangle=1200x800+0+0 center=true
-bspc rule -a 'hotkeyhub' state=floating rectangle=1200x800+0+0 center=true
-bspc rule -a 'qalculate-gtk' state=floating rectangle=850x560+0+0 center=true
-bspc rule -a 'pavucontrol' state=floating rectangle=920x450+0+0 center=true
+bspc rule -a 'org.gnome.FileRoller' state=floating rectangle=$(rect 63 74) center=true
+bspc rule -a 'Gnome-calculator' state=floating rectangle=$(rect 19 47) center=true
+bspc rule -a 'loupe' state=floating rectangle=$(rect 63 74) center=true
+bspc rule -a 'hotkeyhub' state=floating rectangle=$(rect 63 74) center=true
+bspc rule -a 'qalculate-gtk' state=floating rectangle=$(rect 45 55) center=true
+bspc rule -a 'pavucontrol' state=floating rectangle=$(rect 48 42) center=true
