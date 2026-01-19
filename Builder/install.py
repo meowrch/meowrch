b4d66f7293bd12618dfc58a9623c3964a54e171e
DIMFLIX <dimflix.official@gmail.com>
2026-01-19 13:23:38 +0300

    feat: updated meowrch to version 3.0.2

--- Builder/install.py
+++ Builder/install.py
@@ -96,7 +96,7 @@ class Builder:
             self.daemons_setting()
             PostInstallation.apply(self.build_options)
 
-            self._write_installation_metadata("3.0.1")
+            self._write_installation_metadata("3.0.2")
 
             logger.warning(
                 "The script was unable to automatically install these packages."
