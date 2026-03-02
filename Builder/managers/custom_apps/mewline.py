7ada3e8daf893a8864689141cfcd3a239eb7fcba
DIMFLIX <dimflix.official@gmail.com>
2026-03-02 23:03:45 +0300

    fix: Changed the method of locking the screen via mewline

--- Builder/managers/custom_apps/mewline.py
+++ Builder/managers/custom_apps/mewline.py
@@ -1,7 +1,10 @@
+import os
+import json
 import subprocess
 import traceback
 
 from loguru import logger
+from pathlib import Path
 
 from .base import AppConfigurer
 from ..package_manager import PackageManager
@@ -11,6 +14,7 @@ class MewlineConfigurer(AppConfigurer):
     def setup(self) -> None:
         if PackageManager.check_package_installed("mewline"):
             try:
+                self._create_default_config()
                 self._create_hotkeys()
             except Exception:
                 logger.error(f"Mewline main setup error: {traceback.format_exc()}")
@@ -26,3 +30,31 @@ class MewlineConfigurer(AppConfigurer):
             text=True,
         )
         logger.success("Mewline keybindings configured successfully!")
+
+    def _create_default_config(self) -> None:
+        """Логика создания дефолтной конфигурации mewline"""
+        logger.info("Configuring mewline...")
+
+        subprocess.run(
+            ["mewline", "--generate-default-config"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        
+        config_path = Path.home().joinpath(".config/mewline/config.json")
+        if config_path.exists():
+            with open(config_path) as f:
+                config = json.load(f)
+            
+            config["modules"]["dynamic_island"]["power_menu"]["commands"]["bspwm"]["lock"] = (
+                f"sh {os.environ['HOME']}/.local/bin/screen-lock.sh"
+            )
+
+            with open(config_path, "w") as f:
+                json.dump(config, f, indent=4, ensure_ascii=False)
+
+            logger.success("Mewline config created!")
+        else:
+            logger.error("Failed to create mewline config...")
